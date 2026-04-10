a47eec0cc0747ea70b91d0e6c71d75534269e6eb
marlena <m.k.kropidlowska@gmail.com>
2026-04-10 16:51:47 +0200

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,9 +85,16 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  manifest: "/site.webmanifest",
   icons: {
-    icon: [{ url: "/logo-zielone-serce.png", type: "image/png" }],
-    apple: [{ url: "/logo-zielone-serce.png", type: "image/png" }],
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-48x48.jpg", sizes: "48x48", type: "image/jpeg" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
   },
 };
 
