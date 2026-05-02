f8836adf8a4afffeff19b2a8c29c1c67dfa627ac
marlena <m.k.kropidlowska@gmail.com>
2026-05-02 21:30:13 +0200

    Add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { DM_Serif_Display, Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -147,6 +148,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
